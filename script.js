d5385206c56f166f1c7a36ffcadf54a08eaa7fc1
Bang <bang_an@outlook.com>
2024-01-16 17:44:29 -0600

    Add remove book and toggle read status logic

--- script.js
+++ script.js
@@ -7,11 +7,6 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
-function render(){
-    
-}
-
-
 function addBookToLibrary() {
   let bookTitle = document.querySelector("#title").value;
   let bookAuthor = document.querySelector("#author").value;
@@ -19,9 +14,18 @@ function addBookToLibrary() {
   let bookRead = document.querySelector("#read").checked;
   let newBook = new Book(bookTitle, bookAuthor, bookPage, bookRead);
   myLibrary.push(newBook);
-//   function render();
+  render();
 }
 
+function removeBook(index){
+  myLibrary.splice(index, 1);
+  render();
+}
+
+function changeReadStatus(index){
+  myLibrary[index].read = !myLibrary[index].read;
+  render();
+}
 
 const addBkBtn = document.querySelector("#add-bk-btn");
 const bookForm = document.querySelector("#book-form");
@@ -34,3 +38,21 @@ bookForm.addEventListener("submit", (event)=>{
     event.preventDefault();
     addBookToLibrary();
 })
+
+function render(){
+  let library = document.getElementById("library");
+  document.getElementById("library").innerHTML = "";
+  for(let index = 0; index < myLibrary.length; index++){
+    let bookElement = document.createElement('div');
+    let book = myLibrary[index];
+    bookElement.innerHTML = `
+    <p>${book.title}</p>
+    <p>${book.author}</p>
+    <p>${book.pages}</p>
+    <p>${book.read ? "read" : "Not read"}</p>
+    <button id="rm-bk-btn" onclick="removeBook(${index})">Remove Book</button>
+    <button id="rm-bk-btn" onclick="changeReadStatus(${index})">Change Status</button>
+    `;
+    library.appendChild(bookElement);
+  }
+}
